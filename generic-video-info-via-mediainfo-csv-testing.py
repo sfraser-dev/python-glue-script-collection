a9182710eb07d3b3f0ef941d53eee63246faa7b7
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-12-05 14:57:17 +0000

    another perl to python conversion working (eventually!)

---
+++ generic-video-info-via-mediainfo-csv-testing.py
@@ -0,0 +1,13 @@
+import subprocess
+
+file_path = ".\sample-10s.mp4"  # Replace with the actual file path
+
+media_info_output_duration = subprocess.check_output(
+    ["MediaInfo.exe", "-f", "--Inform=General;%Duration%", file_path],
+    universal_newlines=True,
+    shell=True,
+)
+media_duration = float(media_info_output_duration.strip()) / 1000
+
+print(media_info_output_duration)
+print(media_duration)
--- generic-video-info-via-mediainfo-csv.py
+++ generic-video-info-via-mediainfo-csv.py
@@ -1,63 +1,107 @@
 import subprocess
-import logging
+import csv
 import os
-import pathlib
+from pathlib import Path
+from datetime import timedelta
+import logging
 
-# Create a logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler('script.log')
-logger.addHandler(handler)
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
+# Initialize variables
 file_count = 0
 
-# Find all MPG files in the current directory and its subdirectories
-for root, _, files in os.walk('.'):
-    for file in files:
-        if file.endswith('.mpg'):
-            file_path = os.path.join(root, file)
-            file_count += 1
+# Open CSV file for writing media info
+csv_file_path = 'generic-video-info-via-mediainfo-csv.csv'
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
 
-            # Get media info using MediaInfo CLI
-            media_info_command = ['MediaInfo.exe', '-f', file_path]
-            proc = subprocess.Popen(media_info_command, stdout=subprocess.PIPE)
-            media_info_output, _ = proc.communicate()
-            media_info_lines = media_info_output.decode().strip().split('\n')
+    # Write header row to CSV
+    writer.writerow(['media count', 'media path', 'media name', 'media duration (secs)',
+                     'media bitrate (kbps)', 'video format', 'video resolution', 'video bitrate (kbps)',
+                     'video frame rate (fps)', 'video frame count', 'video duration (secs)'])
 
-            # Extract relevant media info
-            media_duration = None
-            media_bitrate = None
-            video_format = None
-            video_resolution = None
-            video_bitrate = None
-            video_frame_rate = None
-            video_frame_count = None
-            video_duration = None
+    # Iterate through current directory and subdirectories
+    for root, _, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.mp4'):
+                file_count += 1
 
-            for line in media_info_lines:
-                key, value = line.split(': ')
-                if key == 'General;%Duration%':
-                    media_duration = int(value) / 1000
-                elif key == 'General;%BitRate%':
-                    media_bitrate = int(value)
-                elif key == 'Video;%Format%':
-                    video_format = value
-                elif key == 'Video;%Width%':
-                    video_width = int(value)
-                elif key == 'Video;%Height%':
-                    video_height = int(value)
-                    video_resolution = f'{video_width}x{video_height}'
-                elif key == 'Video;%BitRate%':
-                    video_bitrate = int(value)
-                elif key == 'Video;%FrameRate%':
-                    video_frame_rate = float(value)
-                elif key == 'Video;%FrameCount%':
-                    video_frame_count = int(value)
-                elif key == 'Video;%Duration%':
-                    video_duration = int(value) / 1000
+                # Construct file path
+                file_path = os.path.join(root, file)
 
-            # Log the media info
-            logger.info(f"{file_count}, {file_path}, {media_duration}, {media_bitrate}, {video_format}, {video_resolution}, {video_bitrate}, {video_frame_rate}, {video_frame_count}, {video_duration}")
+                # Extract media info using MediaInfo CLI
+                media_info_output_duration = subprocess.check_output(
+                    ["MediaInfo.exe", "-f", "--Inform=General;%Duration%", file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                media_duration = float(media_info_output_duration.strip()) / 1000
 
-logging.info(f"Total MPG files processed: {file_count}")
 
+                media_info_output_bitrate = subprocess.check_output(
+                    ['MediaInfo.exe', '-f', '--Inform=General;%BitRate%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                media_bitrate = int(media_info_output_bitrate.strip())
+
+                media_info_output_format = subprocess.check_output(
+                    ['MediaInfo.exe', '-f', '--Inform=Video;%Format%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                video_format = media_info_output_format.strip()
+
+                media_info_output_width = subprocess.check_output(
+                    ['MediaInfo.exe', '-f', '--Inform=Video;%Width%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                video_width = int(media_info_output_width.strip())
+
+                media_info_output_height = subprocess.check_output(
+                    ['MediaInfo.exe', '-f', '--Inform=Video;%Height%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                video_height = int(media_info_output_height.strip())
+
+                video_resolution = f'{video_width}x{video_height}'
+
+                media_info_output_bitrate_video = subprocess.check_output([
+                    'MediaInfo.exe', '-f', '--Inform=Video;%BitRate%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                video_bitrate = int(media_info_output_bitrate_video.strip())
+
+                media_info_output_frame_rate = subprocess.check_output([
+                    'MediaInfo.exe', '-f', '--Inform=Video;%FrameRate%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                video_frame_rate = float(media_info_output_frame_rate.strip())
+
+                media_info_output_frame_count = subprocess.check_output([
+                    'MediaInfo.exe', '-f', '--Inform=Video;%FrameCount%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                video_frame_count = int(media_info_output_frame_count.strip())
+
+                media_info_output_duration_video = subprocess.check_output([
+                    'MediaInfo.exe', '-f', '--Inform=Video;%Duration%', file_path],
+                    universal_newlines=True,
+                    shell=True,
+                )
+                video_duration = float(
+                    media_info_output_duration_video.strip()) / 1000
+
+                # Write media info to CSV
+                writer.writerow([file_count, file_path, Path(file).name, media_duration, media_bitrate,
+                                 video_format, video_resolution, video_bitrate, video_frame_rate,
+                                 video_frame_count, video_duration])
+
+logging.info(f"Total MP4 files processed: {file_count}")
+logging.info(f"Media info written to {csv_file_path}")
